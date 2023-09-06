749442d36fec3f9114dc74452f8db748d46e5ede
Caju Golden <ricardo.alve100@gmail.com>
2023-09-06 11:40:06 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct Sistema{
+    int ID; //identificador
+    char NAME[45];
+    char USER[45];
+    char PASS[45];
+    int PERMISSION;//permissões 1-total 2-parcial 3-somente busca
+    int ACTIVATED;// 0-Excluido 1-Ativo
+
+}Sistema;
+
+Sistema* create_sistema() {
+    Sistema *sistema = (Sistema*)malloc(1*sizeof(Sistema));
+    return sistema;
+}
+
+int add_sistema(int id,char name[45], char user[45], char password[45],int qtd_users,Sistema **sistema){
+
+
+
+
+        *sistema = (Sistema *)realloc(*sistema, (qtd_users + 1) * sizeof(Sistema));
+        if(*sistema == NULL){    return -1; }
+
+
+    (*sistema)[id].ID = qtd_users +1;
+    strcpy((*sistema)[id].NAME,name);
+    strcpy((*sistema)[id].USER,user);
+    strcpy((*sistema)[id].PASS,password);
+
+
+    return qtd_users+1;
+
+
+}
+
+
+int main() {
+
+    int exit_menu_1 = 1; //sair do menu do usuario logado
+    int exit_menu_0 = 1; //sair do menu do cadastro de usuario
+    int chosen ;// escolhas do menu
+    int log; // mensagens de erro ou sucesso
+    int num_sistema_ids = 0 ;
+
+
+
+    char nome_sistema[45], user_sistema[45], pass_sistema[45];
+
+
+    Sistema *sistema_p = create_sistema();
+
+    //add_sistema(sistema_p);
+    //sistema_p = (Sistema *) malloc(2*sizeof(Sistema));
+    //printf("Sistema tamanho %d\n", sizeof(sistema_p));
+
+    while(exit_menu_0 != 0){
+        setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+        printf("\t \t -MENU-\n");
+        printf("\t  1- Cadastrar\n");
+        printf("\t  2- Logar\n");
+        printf("\t  3- Excluir Usuário\n");
+        printf("\t  4-Exit\n");
+        scanf("%d", &chosen);
+
+        switch (chosen) {
+            case 1: // cadatrar usuário
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+                printf("Digite o nome Funcionário\n");
+                fgets(nome_sistema, sizeof(nome_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+
+                printf("Digite o User \n");
+                fgets(user_sistema, sizeof(user_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+
+                printf("Digite a senha\n");
+                fgets(pass_sistema, sizeof(pass_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+
+
+                num_sistema_ids = add_sistema(num_sistema_ids,nome_sistema,user_sistema,pass_sistema,num_sistema_ids,&sistema_p);
+                break;
+            case 2: //logar, isso entrá em outro menu já logado
+
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+                printf("Digite o User \n");
+                fgets(user_sistema, sizeof(user_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+
+                printf("Digite a senha\n");
+                fgets(pass_sistema, sizeof(pass_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+                for(int i=0 ; i <= num_sistema_ids; i++){
+                    if(strcmp(user_sistema,sistema_p[i].USER) ==0 ){
+                        if(strcmp(pass_sistema,sistema_p[i].PASS)== 0){
+                            printf("Login Aprovado\n");
+                            while(exit_menu_1 != 0){
+
+                                printf("\t \t -MENU-\n");
+                                printf("\t  9-Config\n");
+                                printf("\t  10-Exit\n");
+                                scanf("%d", &chosen);
+
+                                switch (chosen) {
+                                    case 9: // escolha de config
+                                        printf("\t \t -MENU-\n");
+                                        printf("\t  1-TXT\n");
+                                        printf("\t  2-BIN\n");
+                                        scanf("%d", &chosen);
+                                        switch (chosen) {
+                                            case 1:
+                                                //chamar função de salvar em txt
+                                                break;
+                                            case 2:
+                                                //chamar função de salvar em bin
+                                                break;
+                                            default:
+                                                // por padrão salva em bin
+                                                break;
+                                        }
+                                        break;
+                                    case 10:
+                                        printf("Exit Success\n");
+                                        exit_menu_1 = 0;
+                                        break;
+
+                                }
+
+                            }
+                        } else{
+                            printf("Senha incorreta\n");
+                        }
+                        break;
+                    } else if(strcmp(user_sistema,sistema_p[i].USER) ==1 && i == num_sistema_ids){
+                        printf("Usuário incorreto\n");
+                    }
+                }
+
+
+
+                break;
+
+            case 3://Excluir
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+                printf("Digite o User \n");
+                fgets(user_sistema, sizeof(user_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+
+
+                printf("Digite a senha\n");
+                fgets(pass_sistema, sizeof(pass_sistema), stdin);
+                setbuf(stdin, NULL);// limpando buffer para não pegar  resto de uma entrada passada
+                for(int i=0 ; i <= num_sistema_ids; i++){
+                    if(strcmp(user_sistema,sistema_p[i].USER) ==0 ){
+                        if(strcmp(pass_sistema,sistema_p[i].PASS)== 0){
+                            sistema_p[i].ACTIVATED=0;
+                            printf("Usuário exlcuido com sucesso\n");
+                        } else{
+                            printf("Senha incorreta");
+                        }
+                        break;
+                    } else if(strcmp(user_sistema,sistema_p[i].USER) ==1 && i == num_sistema_ids){
+                        printf("Usuário não encontrado");
+                    }
+                }
+                break;
+
+            case 4://exit
+                printf("Exit Success\n");
+                exit_menu_0 = 0 ;
+                break;
+
+        }
+
+    }
+
+printf("----------------------------------nome_sistema (%s)---------------------------------------------------------",sistema_p[0].NAME);
+
+
+
+
+
+    return 0;
+}
